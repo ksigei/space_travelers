0a87b55ac75b2baee7b07982eac0962c5cebca50
Ksigei <sigeikiprono4@gmail.com>
2022-05-24 22:52:36 +0300

    Style the missions page

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import getMissionsAction from '../redux/missions/actions';
+import './css/Mission.css';
 
 const Missions = () => {
   const dispatch = useDispatch();
@@ -8,26 +9,31 @@ const Missions = () => {
     dispatch(getMissionsAction());
   }, []);
   const missions = useSelector((state) => state.missionReducers);
-  return missions.map((mission) => (
-    <main className="box" key={mission.missionId}>
-      <div className="left">
-        {/* <div className="mission-img">
-          <img src={mission.missionImgUrl} alt="mission" />
-        </div> */}
-      </div>
-      <div className="right">
-        <h4>{mission.missionName}</h4>
-        <p>{mission.missionDescription}</p>
-        <button
-          type="button"
-          className="btn btn-primary btn-md"
-        >
-          Join mission
-
-        </button>
-      </div>
+  return (
+    <main>
+      <table>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+        {missions.map((mission) => (
+          <tr key={mission.missionId}>
+            <td>{mission.missionName}</td>
+            <td>{mission.missionDescription}</td>
+            <td>
+              <div className="status">
+                <h3>Not a member</h3>
+                <button type="button" className="btn btn-primary btn-md">
+                  Join mission
+                </button>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </table>
     </main>
-  ));
+  );
 };
 
 export default Missions;
