919180abe1eafd562debe25a70254606a777bcb2
HamzaAnwar1998 <anwarhamza919@gmail.com>
2022-05-25 16:45:48 +0500

    complete navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logoUrl from '../assets/planet.png';
 import './css/Header.css';
 
 function Header() {
   return (
-    <nav>
+    <nav className="navbox">
       <Link className="icon" to="/">
         <img src={logoUrl} alt="planet" />
+        <h2>Space Travelers Hub</h2>
       </Link>
-      <Link to="/missions">Missions</Link>
-
+      <div className="rightside">
+        <NavLink to="/" className="link">Rockets</NavLink>
+        <NavLink to="/missions" className="link">Missions</NavLink>
+        <NavLink to="/profile" className="link">Profile</NavLink>
+      </div>
     </nav>
   );
 }
