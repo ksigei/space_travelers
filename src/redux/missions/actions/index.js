9b5b53b4220a2a8e1204432a24354525f23cedeb
HamzaAnwar1998 <anwarhamza919@gmail.com>
2022-05-25 16:36:09 +0500

    add join missions

--- src/redux/missions/actions/index.js
+++ src/redux/missions/actions/index.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 export const GET_MISSIONS = 'SPACE-TRAVELERS/missions/GET_MISSIONS';
+export const JOIN_MISSIONS = 'SPACE-TRAVELERS/missions/JOIN_MISSIONS';
 
-const getMissionsAction = () => async (dispatch) => {
+export const getMissionsAction = () => async (dispatch) => {
   const missions = await axios.get('https://api.spacexdata.com/v3/missions');
   const data = await missions.data;
   const missionsArray = [];
@@ -20,4 +21,9 @@ const getMissionsAction = () => async (dispatch) => {
   });
 };
 
-export default getMissionsAction;
+export const joinMissionAction = (payload) => ({
+  type: JOIN_MISSIONS,
+  payload,
+});
+
+export default { getMissionsAction, joinMissionAction };
--- src/redux/missions/reducers/missionReducers.js
+++ src/redux/missions/reducers/missionReducers.js
@@ -1,4 +1,7 @@
-import { GET_MISSIONS } from '../actions';
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
+/* eslint-disable no-case-declarations */
+import { GET_MISSIONS, JOIN_MISSIONS } from '../actions';
 
 const initialState = {
   missions: [],
@@ -8,6 +11,13 @@ const missionReducers = (state = initialState.missions, action) => {
   switch (action.type) {
     case GET_MISSIONS:
       return action.payload;
+    case JOIN_MISSIONS:
+      return state.map((mission) => {
+        if (mission.missionId === action.payload) {
+          return { ...mission, missionJoined: true };
+        }
+        return mission;
+      });
     default:
       return state;
   }
