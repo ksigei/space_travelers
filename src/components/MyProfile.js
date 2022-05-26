a1463f87a27b7067ad0393ae56ea3743efac7ebd
Ksigei <sigeikiprono4@gmail.com>
2022-05-26 11:13:33 +0300

    Display reserved rockets in profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+function MyProfile() {
+  /* eslint-disable */ 
+  const reservedRockets = useSelector((state) => state.rocketReducers.filter((rocket) => rocket.rocketBooked));
+  /* eslint-enable */
+  return (
+    <div>
+      {reservedRockets.map((rocket) => (
+        <div key={rocket.id}>
+          <h3>{rocket.rocketName}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default MyProfile;
